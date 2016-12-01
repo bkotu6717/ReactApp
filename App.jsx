b5200906326ee8ce4681dede6401da3cb2925a05
railsfactory-bhaskararao <bhaskararao@railsfactory.org>
2016-12-01 11:47:08 +0530

    created different types of componets and require them inside main component

--- App.jsx
+++ App.jsx
@@ -8,16 +8,37 @@ class App extends React.Component {
       }
 
       return (
-         <div>
-			<h1> Header !!</h1>
-			<h1> Content !! </h1>
-			<h1> Footer !! </h1>
-			<p data-myattribute="somevalue"> This is p tag inside App Component !! </p>	
-			<h1> This is expression {1 + 1 + 1} </h1>
-			<p style={myStyle}> this p tag is having style applied</p>
-         </div>
+      	<div>
+	      	<Header/>
+	      	<Content/>
+	      	<Footer/>
+	    </div>
       );
    }
+}
+
+class Header extends React.Component {
+	render(){
+		return(
+			<h1> This is Header Content </h1>
+		);
+	};
+}
+
+class Content extends React.Component {
+	render(){
+		return(
+			<h1> This is body Content </h1>
+		)
+	}
+}
+
+class Footer extends React.Component {
+	render(){
+		return(
+			<h1> This is Footer content </h1>
+		)
+	}
 }
 
 export default App;
