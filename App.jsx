18882d37f5d0e4c467d6ba2dd9bccc48dbdd4122
railsfactory-bhaskararao <bhaskararao@railsfactory.org>
2016-12-01 19:02:19 +0530

    Working with react forms

--- App.jsx
+++ App.jsx
@@ -37,6 +37,7 @@ class App extends React.Component {
 	      	<ForceUpdateHandler/>
 	      	<FindDomNode/>
 	      	<Lifecycle/>
+	      	<FormComponent/>
 	    </div>
       );
    	}
@@ -238,6 +239,29 @@ class Lifecycle extends React.Component{
 		)	
 	}
 
+}
+
+class FormComponent extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			data: 'initial data!!'
+		}
+	this.updateState = this.updateState.bind(this);
+	}
+
+	updateState(e){
+		this.setState({data: e.target.value})
+	}
+
+	render(){
+		return(
+			<div>
+				<input type="text" value={this.state.data} onChange={this.updateState} />
+				<h4> {this.state.data} </h4>
+			</div>
+		);
+	}
 }
 
 export default App;
