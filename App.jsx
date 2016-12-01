9586f31262fb1a4fd5166109ff80d1a6311a5fb7
railsfactory-bhaskararao <bhaskararao@railsfactory.org>
2016-12-01 12:22:56 +0530

    add  stateless example

--- App.jsx
+++ App.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-   	 var myStyle = {
-         fontSize: 100,
-         color: 'red'
-      }
+	constructor(){
+		super();
+		this.state = {
+			data: [
 
+				{'id': 1,
+				 'name': 'Bhaskar',
+				 'age': 27
+				},
+				{'id': 2,
+				 'name': 'Pavan',
+				 'age': 23
+				},
+				{'id': 3,
+				 'name': 'Jaggu',
+				 'age': 23
+				}
+			]
+		}
+	}
+   	render() {
       return (
       	<div>
 	      	<Header/>
-	      	<Content/>
-	      	<Footer/>
+	      	<table>
+	      		<tbody>
+	      			{this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+	      		</tbody>
+	      	</table>
 	    </div>
       );
-   }
+   	}
 }
 
 class Header extends React.Component {
@@ -25,20 +43,17 @@ class Header extends React.Component {
 	};
 }
 
-class Content extends React.Component {
+class TableRow extends React.Component {
 	render(){
 		return(
-			<h1> This is body Content </h1>
+		<tr>
+			<td> {this.props.data.id}</td>
+			<td> {this.props.data.name}</td>
+			<td>{this.props.data.age}</td>
+		</tr>
 		)
 	}
 }
 
-class Footer extends React.Component {
-	render(){
-		return(
-			<h1> This is Footer content </h1>
-		)
-	}
-}
 
 export default App;
